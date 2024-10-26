4c487fb018ead1313eaabd015f3d3b41460fa830
Joaveinz <joachim.velzel@gmail.com>
2024-10-26 10:22:41 +0200

    add character animation

--- scenes/CharacterBody2D.gd
+++ scenes/CharacterBody2D.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var animationPlayer = $AnimationPlayer
+@onready var sprite = $Sprite2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -16,13 +18,27 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animationPlayer.play("jump_start")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	if(direction != 0):
+		sprite.flip_h = (direction == -1)
+		
+	update_animations(direction)
 	move_and_slide()
+
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animationPlayer.play("idle")
+		else:
+			animationPlayer.play("running")
+	else:
+		animationPlayer.play("jump_loop")
